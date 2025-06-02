fdc4ab31d35d4b463b124bfb4529dd1d8ee66114
Patrick Kuijpers <patrick@9292.nl>
2025-06-02 16:25:04 +0200

    Update version number

--- CountryPicker/build.gradle.kts
+++ CountryPicker/build.gradle.kts
@@ -54,7 +54,7 @@ afterEvaluate {
             create<MavenPublication>("release") {
                 groupId = "com.github.arpitkatiyar1999"
                 artifactId = "countrypicker"
-                version = "2.1.3"
+                version = "2.1.4"
                 from(components["release"])
             }
         }
