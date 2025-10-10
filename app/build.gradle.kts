c161d41a95a5047acc853190540b3eb0dbe921c0
arpitkatiyar1999 <arpitkatiyar2018@gmail.com>
2025-10-10 10:49:04 +0530

    feat: Migrate flag icons to XML drawables and update minSdk
    - Replaced a significant number of PNG flag icons with more scalable and efficient XML vector drawables.
    - Updated the `minSdk` version from 21 to 24 for both the library and the demo app to better support vector assets.
    - Adjusted the width and height attributes in existing XML drawable files from `dp` to `200dp` and `150dp` for standardization.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.arpitkatiyarprojects.countrypickerproject"
-        minSdk = 21
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
